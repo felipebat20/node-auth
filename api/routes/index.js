9cd3688cf8bab692b9515ccabe2780c9ce22db45
felipebat20 <felipebatista24680@gmail.com>
2023-08-19 08:16:12 -0300

    create: user model

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser')
- 
+
 const produto = require('./produtoRoute')
+const user = require('./user')
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
-    produto
+    produto,
+    user,
   )
 }
