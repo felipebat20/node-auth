bb2433612d3dac21cadfb2805bbe7848a5858308
felipebat20 <felipebatista24680@gmail.com>
2023-09-19 22:53:28 -0300

    create: user permission and user roles

--- api/services/SecurityService.js
+++ api/services/SecurityService.js
@@ -1,3 +1,5 @@
+const Sequelize = require('sequelize');
+
 const db = require('../models');
 
 class SecurityService {
@@ -18,7 +20,49 @@ class SecurityService {
       where: { id: dto.userId },
     });
 
-    return user;
+    if (! user) {
+      throw new Error('User not found');
+    }
+
+    const roles = await db.roles.findAll({
+      where: {
+        id: {
+          [Sequelize.Op.in]: dto.roles,
+        },
+      },
+    });
+
+    const permissions = await db.permissions.findAll({
+      where: {
+        id: {
+          [Sequelize.Op.in]: dto.permissions,
+        },
+      },
+    });
+
+    await user.removeUser_role(user.user_roles);
+    await user.removeUser_permission(user.user_permissions);
+
+    await user.addUser_role(roles);
+    await user.addUser_permission(permissions);
+
+    const new_user = await db.users.findOne({
+      include: [
+        {
+          model: db.roles,
+          as: 'user_role',
+          attributes: ['id', 'name', 'description'],
+        },
+        {
+          model: db.permissions,
+          as: 'user_permission',
+          attributes: ['id', 'name', 'description'],
+        }
+      ],
+      where: { id: dto.userId },
+    });
+
+    return new_user;
   }
 }
 
